ba70f6b2150cb1093f26cdb2d77754a32f9a20ae
Simon Lydell <simon.lydell@gmail.com>
2017-11-04 09:57:38 +0100

    Add no-return-await

--- index.js
+++ index.js
@@ -96,6 +96,7 @@ module.exports = {
     "no-redeclare": "error",
     "no-regex-spaces": "error",
     "no-return-assign": "error",
+    "no-return-await": "error",
     "no-script-url": "error",
     "no-self-assign": "error",
     "no-self-compare": "error",
